380f43b6bb9b787643743e98bf82150fdfcb9317
xushijie <983869721@qq.com>
2018-09-19 14:19:55 +0800

    golang 方法调用的两种方式

---
+++ fmtTest/fmtTest.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Integer int
+
+func(a *Integer)ToPrint(){
+	fmt.Println(a)
+}
+
+func main () {
+	var a Integer
+	a.ToPrint() //方法值形式 (method value)
+	(*Integer).ToPrint(&a) //方法表达式形式 (method expression) 
+}
