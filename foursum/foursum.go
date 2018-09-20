56d34d5623c8c0086e7decedc4f910a81ddeee20
xushijie <983869721@qq.com>
2018-09-20 09:51:41 +0800

    格式化

--- foursum/foursum.go
+++ foursum/foursum.go
@@ -7,58 +7,58 @@ import (
 
 type Integer []int
 
-func (c Integer)Len()int  {
+func (c Integer) Len() int {
 	return len(c)
 }
 
-func (c Integer)Less(i,j int)bool  {
+func (c Integer) Less(i, j int) bool {
 	return c[i] < c[j]
 }
 
-func (c Integer)Swap(i,j int)  {
-	c[i],c[j] = c[j],c[i]
+func (c Integer) Swap(i, j int) {
+	c[i], c[j] = c[j], c[i]
 }
 
 func main() {
 
-	array := Integer{10,20,30,40,-3,-9,-20,3,9}
+	array := Integer{10, 20, 30, 40, -3, -9, -20, 3, 9}
 	target := 0
 
 	var result [][4]int
 
 	sort.Sort(array)
 
-	for i := 0; i < len(array) - 3 ; i++  {
-		if i > 0 && array[i] == array[i-1]{
+	for i := 0; i < len(array)-3; i++ {
+		if i > 0 && array[i] == array[i-1] {
 			continue
 		}
-		for j:=i+1; j< len(array) - 2 ; j++ {
-			if(j > i+1 && array[j] == array[j-1]){
+		for j := i + 1; j < len(array)-2; j++ {
+			if j > i+1 && array[j] == array[j-1] {
 				continue
 			}
-			k := j +1
+			k := j + 1
 			l := len(array) - 1
 			for k < l {
-				sum :=  array[i] + array[j] + array[k] + array[l]
-				if(sum < target){
+				sum := array[i] + array[j] + array[k] + array[l]
+				if sum < target {
 					k++
 					for k < l && array[k] == array[k-1] {
 						k++
 					}
-				}else if(sum > target){
+				} else if sum > target {
 					l--
 					for k < l && array[l] == array[l+1] {
 						l--
 					}
-				}else{
-					fourInt := [4]int{array[i],array[j],array[k],array[l]}
+				} else {
+					fourInt := [4]int{array[i], array[j], array[k], array[l]}
 					result = append(result, fourInt)
 					k++
 					l--
 					for k < l && array[l] == array[l+1] {
 						l--
 					}
-					for k < l && array[k] == array[k-1]{
+					for k < l && array[k] == array[k-1] {
 						k++
 					}
 				}
